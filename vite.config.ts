cac70ed2800b8051efd20596a4b5b51341760b0e
Aura44422 <oai052706@gmail.com>
2025-09-25 18:02:48 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Serving from repository Pages: https://aura44422.github.io/noviy-center21.github.io/
-  base: '/noviy-center21.github.io/'
-})

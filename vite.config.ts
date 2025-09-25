9f50f908ed16d12573c94aecef5c88ab974c0e66
Aura44422 <oai052706@gmail.com>
2025-09-25 17:56:59 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Serving from user site root (https://aura44422.github.io/)
-  base: '/'
+  // Serving from repository Pages: https://aura44422.github.io/noviy-center21.github.io/
+  base: '/noviy-center21.github.io/'
 })
